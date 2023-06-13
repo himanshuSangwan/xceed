e1b634fdb981f600adf64a286e5bc1bcea94f0fd
himanshu_sangwan <h.sangwan22@gmail.com>
2023-06-13 12:04:54 +0530

    lesson fix

--- front-end/src/pages/lesson/Lesson.js
+++ front-end/src/pages/lesson/Lesson.js
@@ -74,7 +74,6 @@ export default function Lesson() {
     setQuizzeMarks({ ...quizzeResult });
   };
   const submitResult = async (id, detail) => {
-    console.log(id, quizzeMarks[id], detail);
     let obj = {
       quizzeScore: quizzeMarks[id],
       quizzeId: detail._id,
@@ -140,65 +139,6 @@ export default function Lesson() {
                     ) : (
                       <div>
                         <p className="quelifiedTxt startTxt">Let's have a quick quizze</p>
-                        {/* <OwlCarousel
-                          className="owl-carousel owl-carousel-custom owl-theme lessonOwlCarousel slideshow-container"
-                          loop={false}
-                          margin={20}
-                          responsiveClass={true}
-                          dots={false}
-                          nav={false}
-                          slideBy={1}
-                          responsive={{
-                            0: {
-                              items: 1,
-                            },
-                            576: {
-                              items: 1,
-                            },
-                            768: {
-                              items: 1,
-                            },
-                          }}
-                          startPosition={startPosition.current}
-                          onDragged={({ item, page }) => {
-                            startPosition.current = item.index;
-                          }}
-                        >
-                          {itm.quizze.questions.map((i, j) => {
-                            return (
-                              <div className="mySlides d-block">
-                                <div className="numbertext">
-                                  {j + 1} / {itm.quizze.questions.length}
-                                </div>
-                                <div className="Content">
-                                  <h3>{i.question}</h3>
-                                  <div className="anss">
-                                    <form action>
-                                      {i.option.map((opt) => {
-                                        return (
-                                          <div>
-                                            <input
-                                              type="radio"
-                                              id={opt}
-                                              name={i._id}
-                                              defaultValue={opt}
-                                              onChange={(e) =>
-                                                i.correct_answers.includes(e.target.value)
-                                                  ? handleQuizze(itm._id, i.marks)
-                                                  : handleQuizze(itm._id, -i.marks)
-                                              }
-                                            />
-                                            <label htmlFor={opt}>{opt}</label>
-                                          </div>
-                                        );
-                                      })}
-                                    </form>
-                                  </div>
-                                </div>
-                              </div>
-                            );
-                          })}
-                        </OwlCarousel> */}
                         <Carousel itm={itm} handleQuizze={handleQuizze} />
                         <div className="loginBtn mt-4">
                           <button
